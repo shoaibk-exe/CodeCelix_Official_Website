6dd889f5f3d7d5df30b856a54e46d6742c4e655f
Muhammad Qasim Ali <muhammad.bsse4392@iiu.edu.pk>
2025-07-24 15:56:21 +0500

    Update App component to use Footer with logo prop

--- CodeCelix_Official_Website/src/App.jsx
+++ CodeCelix_Official_Website/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css';
 import Header from './components/Header';
 import { Outlet } from 'react-router';
 import Logo from '/Logo/CodeCelix_Logo.jpg';
+import Footer from './components/Footer';
 
 function App() {
   return (
